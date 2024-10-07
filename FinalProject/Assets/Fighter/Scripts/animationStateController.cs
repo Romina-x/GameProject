dec1078cc32dcd32b1725bd4d6802ef278622a2c
Romina Petrozzi <zkac336@live.rhul.ac.uk>
2024-10-07 21:24:19 +0100

    Configured running animation & transitions

--- FinalProject/Assets/Fighter/Scripts/animationStateController.cs
+++ FinalProject/Assets/Fighter/Scripts/animationStateController.cs
@@ -14,13 +14,37 @@ public class animationStateController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        bool isRunning = animator.GetBool("isRunning");
         bool isWalking = animator.GetBool("isWalking");
         bool forwardPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
+
         if (!isWalking && forwardPressed){
             animator.SetBool("isWalking", true);
         }
         if (isWalking && !forwardPressed){
             animator.SetBool("isWalking", false);
+        }
+
+        if (!isRunning && forwardPressed && runPressed)
+        {
+            animator.SetBool("isRunning", true);
+        }
+        else if (isRunning && (!forwardPressed || !runPressed))
+        {
+            // Stop running and either go back to walking or idle
+            animator.SetBool("isRunning", false);
+
+            // If forward key is still pressed, continue walking
+            if (forwardPressed)
+            {
+                animator.SetBool("isWalking", true);
+            }
+            else
+            {
+                // If no forward key is pressed, stop walking as well
+                animator.SetBool("isWalking", false);
+            }
         }
     }
 }

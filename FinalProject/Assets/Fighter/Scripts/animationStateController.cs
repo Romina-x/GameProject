cffcf91e3e1bcc5bcefeb50704e3027ebc0be5e5
Romina Petrozzi <zkac336@live.rhul.ac.uk>
2024-10-08 21:49:38 +0100

    Added animations for jump and attack
    Some animation transitions still choppy

--- FinalProject/Assets/Fighter/Scripts/animationStateController.cs
+++ FinalProject/Assets/Fighter/Scripts/animationStateController.cs
@@ -5,46 +5,60 @@ using UnityEngine;
 public class animationStateController : MonoBehaviour
 {
     Animator animator;
-    // Start is called before the first frame update
+    
     void Start()
     {
         animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Existing walking/running logic
         bool isRunning = animator.GetBool("isRunning");
         bool isWalking = animator.GetBool("isWalking");
         bool forwardPressed = Input.GetKey("w");
         bool runPressed = Input.GetKey("left shift");
 
-        if (!isWalking && forwardPressed){
+        // Walking logic
+        if (!isWalking && forwardPressed)
+        {
             animator.SetBool("isWalking", true);
         }
-        if (isWalking && !forwardPressed){
+        if (isWalking && !forwardPressed)
+        {
             animator.SetBool("isWalking", false);
         }
 
+        // Running logic
         if (!isRunning && forwardPressed && runPressed)
         {
             animator.SetBool("isRunning", true);
         }
         else if (isRunning && (!forwardPressed || !runPressed))
         {
-            // Stop running and either go back to walking or idle
             animator.SetBool("isRunning", false);
-
-            // If forward key is still pressed, continue walking
             if (forwardPressed)
             {
                 animator.SetBool("isWalking", true);
             }
             else
             {
-                // If no forward key is pressed, stop walking as well
                 animator.SetBool("isWalking", false);
             }
         }
+
+
+        // Handle attack animation
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("Attack");
+        }
+
+        // Handle jump animation
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Jump");
+        }
+
     }
 }

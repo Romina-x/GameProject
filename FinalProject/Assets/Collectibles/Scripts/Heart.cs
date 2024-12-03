2129dfcb166c2d0f258ef21821a078df959c0a8b
Romina Petrozzi <zkac336@live.rhul.ac.uk>
2024-12-03 15:08:59 +0000

    Import new VFX files and updated current VFX for enemies and cages

--- FinalProject/Assets/Collectibles/Scripts/Heart.cs
+++ FinalProject/Assets/Collectibles/Scripts/Heart.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class Heart : MonoBehaviour
 {
     [SerializeField] private int _heartHealth = 50; // Amount of health the heart restores
+    [SerializeField] private GameObject collectionVFX; // VFX for collection
 
     private void OnTriggerEnter(Collider other)
     {
         PlayerHealthAndDamage playerHealth = other.GetComponent<PlayerHealthAndDamage>();
+
         if (playerHealth != null)
         {
+            // Update the player's health
             playerHealth.AddHealth(_heartHealth);
+            
+            // Play collection VFX
+            if (collectionVFX != null)
+            {
+                Instantiate(collectionVFX, transform.position, Quaternion.identity);
+            }
+
+            // Remove the heart
             Destroy(gameObject);
         }
     }

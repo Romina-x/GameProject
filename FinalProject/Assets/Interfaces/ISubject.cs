02067f5c875bd83eca0683e3dcae3e05210e9d40
Romina Petrozzi <zkac336@live.rhul.ac.uk>
2024-12-03 19:22:43 +0000

    Created Observer and Subject interfaces

---
+++ FinalProject/Assets/Interfaces/ISubject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public interface ISubject
+{
+    public void RegisterObserver(IObserver observer);
+    public void UnregisterObserver(IObserver observer);
+    public void Notify();
+}
+

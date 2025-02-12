6b75714803401f6b643c79f0fc2127e0ebe625d7
Romina Petrozzi <zkac336@live.rhul.ac.uk>
2025-02-12 15:27:40 +0000

    Created Arrow and RangedAttackRadius scripts and imported ObjectPool classes

---
+++ FinalProject/Assets/CuteGoblins/Scripts/Arrow.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(RigidBody))]
+public class Arrow : PoolableObject
+{
+    private float _autoDestroyTime = 5f;
+    [SerializeField] private float _moveSpeed = 2f;
+    [SerializeField] private int _damage = 10;
+    private RigidBody _rigidBody;
+
+    private const string DISABLE_METHOD_NAME = "Disable";
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<RigidBody>();
+    }
+
+    private void OnEnable()
+    {
+        CancelInvoke(DISABLE_METHOD_NAME);
+        Invoke(DISABLE_METHOD_NAME, _autoDestroyTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IDamageable damageable;
+
+        if (other.TryGetComponent<IDamageable>(out damageable))
+        {
+            damageable.TakeDamage(_damage);
+        }
+
+        Disable();
+    }
+
+    private void Disable()
+    {
+        CancelInvoke(DISABLE_METHOD_NAME);
+        _rigidBody.velocity = Vector3.zero;
+        gameObject.SetActive(false);
+    }
+}

4977c52855061af481c90dd3dcccb254f5116ad8
Romina Petrozzi <zkac336@live.rhul.ac.uk>
2025-04-08 14:49:30 +0100

    Unit tests for object pool complete

---
+++ FinalProject/Assets/Tests/PlayMode/MockPoolableObject.cs
@@ -0,0 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Mock class that inherits from PoolableObject
+public class MockPoolableObject : PoolableObject { }
+
